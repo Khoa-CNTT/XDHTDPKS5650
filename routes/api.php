792921c222b139131c4db167b96991b07ccc77a1
Tran Tan Hung <trantanhung2003@gmail.com>
2025-04-19 20:08:43 +0700

    updated

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Route;
 // Admin
 use App\HTTP\Controllers\Admin\UserController;
 use App\Http\Controllers\RateController;
+use App\Http\Controllers\Staff\CommentController;
 
 Route::get('/list-staff',[UserController::class,'staffList']);
 Route::post('/create-staff',[UserController::class,'staffCreate']);
@@ -22,9 +23,20 @@ Route::post('/create-product',[ProductController::class,'store']);
 Route::get('/edit-product/{id}',[ProductController::class,'edit']);
 Route::post('/edit-product/{id}',[ProductController::class,'update']);
 Route::delete('/delete-product/{id}',[ProductController::class,'destroy']);
-//Customer Rate
+//Comment
+Route::get('/list-comment', [CommentController::class, 'index']);
+Route::post('/create-comment', [CommentController::class, 'store']);
+Route::delete('/delete-comment/{id}', [CommentController::class, 'destroy']);//điều kiện khi bỏ đăng nhập
+
+
+//Customer
+// Rate
 Route::post('/create-rate', [RateController::class, 'store']);// thiếu cái điều kiện invoices có mới tạo được
 Route::delete('/delete-rate/{id}', [RateController::class, 'destroy']);
+//Comment
+Route::get('/list-comment', [CommentController::class, 'index']);
+Route::post('/create-comment', [CommentController::class, 'store']);//thiếu cái điều kiện đăng nhập mới cmt được
+
 
 
 
