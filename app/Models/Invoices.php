c6b841066a5f1e7fa0b9b0940027d7cd50c3ef21
DuyThai <nguyenduythai090603@gmail.com>
2025-05-15 23:17:47 +0700

    Update

--- app/Models/Invoices.php
+++ app/Models/Invoices.php
@@ -17,13 +17,9 @@ class Invoices extends Model
         'address',
         'id_user',
         'id_room',
-<<<<<<< HEAD
         // 'id_order',
-        'firstName',
-        'lastName',
-=======
-        'id_order',
->>>>>>> 97ed8a4e64d8d69dbe2fb679f2bb8952a38ed341
+        // 'firstName',
+        // 'lastName',
         'email',
         'phone',
         'paymentMethod',
@@ -31,30 +27,24 @@ class Invoices extends Model
         'total',
         'type'
     ];
-<<<<<<< HEAD
     public function user()
     {
         return $this->belongsTo(User::class, 'id_user');
     }
 
-=======
 
     public function services()
     {
         return $this->belongsToMany(Service::class, 'invoice_services', 'invoice_id', 'service_id');
     }
     
->>>>>>> 97ed8a4e64d8d69dbe2fb679f2bb8952a38ed341
     public function room()
     {
         return $this->belongsTo(Room::class, 'id_room');
     }
 
-<<<<<<< HEAD
     // public function order()
     // {
     //     return $this->belongsTo(::class, 'id_order');
     // }
-=======
->>>>>>> 97ed8a4e64d8d69dbe2fb679f2bb8952a38ed341
 }
